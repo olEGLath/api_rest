6d0f79308b62d876d8bf229d6db1d5e8de31be0a
olEGLath <esdrasdaniel1016@gmail.com>
2025-09-06 09:36:36 -0300

    Fixing api_rest

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,17 @@
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('foto');
+
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ errors: [err.code] });
+      }
+      return res.json({ url: req.file.path });
+    });
   }
 }
 
