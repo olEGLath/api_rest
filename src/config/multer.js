c3de430ac46cbd845eb0ba4736d39e4b7754c891
olEGLath <esdrasdaniel1016@gmail.com>
2025-09-07 23:48:59 -0300

    Fixing api_rest

--- src/config/multer.js
+++ src/config/multer.js
@@ -5,8 +5,8 @@ const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      cb(new multer.MulterError('Arquivo precisa ser JPEG ou PNG'));
+    if (file.mimetype === "image/png" || file.mimetype === "image/jpg") {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
     }
 
     return cb(null, true);
